9258312e295eec568f9b41bc59dbf2976dba4e88
MarianaDMG05git <mariana.gomes87@etec.sp.gov.br>
2024-04-09 20:39:35 -0300

    FINALIZAÇÃO DA CAMADA DE DADOS E INSTALAÇÃO DE PACOTES

---
+++ GalloFlix/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using GalloFlix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalloFlix.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Genre> Genres { get; set; }
+}
